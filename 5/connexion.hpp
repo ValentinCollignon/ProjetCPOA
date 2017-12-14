564dd6555009cf69974411d0ad89c03b33267674
Brice Losson <brice.losson7@etu.univ-lorraine.fr>
2017-12-14 17:12:05 +0100

    Début de la connexion

--- 5/connexion.hpp
+++ 5/connexion.hpp
@@ -1,8 +1,27 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef CONNEXION_H
+#define CONNEXION_H
 
+#include <iostream>
+#include <string>
+#include <vector>
 
+/* Liste des classes */
+class Utilisateur
+{
+private:
+  std::string pseudo;
+  std::string mdp;
+  int rang; 
+protected:
 
+public:
+  Utilisateur();
+  Utilisateur(std::string login, std::string password, int grade);
+  ~Utilisateur();
+  std::string getPseudo() const;
+  std::string getPassword() const;
+  int getRang() const;
+};
 
 
 #endif
--- 5/console.make
+++ 5/console.make
@@ -1,7 +1,8 @@
 TEST	= proposer_cours.cpp function.cpp cours.cpp
 
-FILES	= header.hpp $(TEST)
+FILES	= cours.hpp $(TEST)
 
 proposer_cours:	$(FILES)
 	g++ -Wall -std=c++11 -ansi -o $@ $(TEST)
 	g++ -Wall -std=c++11 -ansi menu.cpp -o menu
+	g++ -Wall -std=c++11 -ansi utilisateur.cpp connexion.cpp -o connexion
