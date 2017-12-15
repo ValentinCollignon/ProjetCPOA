8e445ce0c69eab3e1b79019d09b8877e320f76ea
Brice Losson <brice.losson7@etu.univ-lorraine.fr>
2017-12-15 13:59:37 +0100

    Fin !

--- 8/gestion_cours.cpp
+++ 8/gestion_cours.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     }
     for (unsigned int i = 0 ; i < tab.size() ; i++)
     {
-      cout << i+1 <<" - " << tab[i].getNom() << endl;
+      cout << tab[i].getNom() << endl;
     }
     fichier.close();
   }
